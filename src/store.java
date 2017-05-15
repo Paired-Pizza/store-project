5018cc57c283de9077d8ea3a4b8eafadfeaedfe4
violet <vdelarosa210@gmail.com>
2017-05-15 14:36:39 -0500

    Completed do/while loop to return to main menu

--- src/store.java
+++ src/store.java
@@ -8,8 +8,11 @@ public class store {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-         int mainMenuSelect = 0;
+        int mainMenuSelect = 0;
 
+        String wantsToReturnToMainMenu;
+
+        do {
 
         System.out.println("Please select an option: ");
         System.out.println("1. Sale");
@@ -18,20 +21,29 @@ public class store {
 
         mainMenuSelect = input.nextInt();
 
-        if (mainMenuSelect == 1){
+        if (mainMenuSelect == 1) {
             shop();
         }
 
-        if (mainMenuSelect == 2){
+        System.out.println("Would you like to go back to the main menu? (y/n)");
+        wantsToReturnToMainMenu = input.next();
+    } while(wantsToReturnToMainMenu.equalsIgnoreCase("y"));
+        if (wantsToReturnToMainMenu.equalsIgnoreCase("n")){
+            System.exit(0);
+        }
+
+        if (mainMenuSelect == 2) {
             // receipt logic here
         }
-        if (mainMenuSelect == 3){
+        if (mainMenuSelect == 3) {
             // exit logic here
             System.out.println("Thank you, come again.");
         }
 
     }
-    public static void shop(){
+
+    public static void shop() {
+
         Scanner input = new Scanner(System.in);
         System.out.println("What would you like to purchase?");
         String item = input.nextLine();
@@ -39,14 +51,10 @@ public class store {
         double price = input.nextDouble();
         System.out.println("How many would you like to buy?");
         int numberOfItems = input.nextInt();
-        System.out.println("Would you like to go back to the main menu?");
-        String mainMenuReturnFromShop = input.nextLine();
-        if(mainMenuReturnFromShop.toLowerCase() == "yes"){
-   //         main();
-        }
 
+}
 
-    //    System.out.println(item + price + numberOfItems);
+//    System.out.println(item + price + numberOfItems);
 
     }
-}
+
