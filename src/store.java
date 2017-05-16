19caf5f1bae17926b2ed5efae298f2e805c86368
Mike Festger <festger@gmail.com>
2017-05-16 14:54:45 -0500

    Added:
    Started over. Most functionality is there.
    
    TO DO:
    
    - Make sure default values return user to menu
    - Make sure multiple items can be stored

--- src/store.java
+++ src/store.java
@@ -1,123 +1,107 @@
-import java.util.Scanner;
-
 /**
- * Created by violet on 5/15/17.
+ * Created by michaelfestger on 5/16/17.
  */
 
+import java.util.Scanner;
 
 public class store {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        String init = "y";
         int mainMenuSelect;
-        String answer = "y";
 
-        String shop = "nothingToPrint";
+        System.out.println("Welcome to the Shop!");
+        showMenu();
+        while (init.equalsIgnoreCase("y")) {
 
-        String wantsToReturnToMainMenu;
+            switch (mainMenuSelect(input)) {
+                case 1:
+                    String item = item(input);
+                    Double price = price(input);
+                    int quantity = quantity(input);
+                    buyAnother(input);
+                    break;
+                case 2:
+                    item = item(input);
+                    price = price(input);
+                    quantity = quantity(input);
+                    double total = price * quantity;
+                    System.out.println("Building receipt...");
+                    System.out.println("Item: " + item);
+                    System.out.println("Price per item: $" + price);
+                    System.out.println("Amount purchased: " + quantity);
+                    System.out.println("Total: $" + total);
+                    buyAnother(input);
+                    break;
+                case 3:
+                    System.out.println("Please come again!");
+                    System.exit(0);
 
-        do {
-
-            System.out.println("Please select an option: ");
-            System.out.println("1. Sale");
-            System.out.println("2. Print Receipt");
-            System.out.println("3. Exit");
-
-            mainMenuSelect = input.nextInt();
-            input.nextLine();
-
-            if (mainMenuSelect == 1) {
-                shop = "Item name | Cost | Quantity\n";
-                Double total = 0.0;
-                do {
-
-
-//                   if(!answer.equalsIgnoreCase("y") || !answer.equalsIgnoreCase("n")){
-//                       System.out.println("wat?");
-//                       answer = breakingYorN(input);
-//                   }
-
-                    switch (answer) {
-                        case "y":
-                            String item = item(input);
-                            Double cost = cost(input);
-                            int quantity = quantity(input);
-                            total += cost * quantity;
-                            shop += item + " | Price: $" + cost + " x " + quantity + "\n";
-                            break;
-                        case "n":
-                            break;
-                        default:
-                            System.out.println("wat?");
-                    }
-
-                    answer = breakingYorN(input);
-
-
-
-                } while (answer.equalsIgnoreCase("y"));
-
-
-                shop += "\n Total: $" + total;
+                default:
+                    System.out.println("Please try that again.");
             }
+            System.out.println("Return to the main menu? (y/n)");
+            init = input.nextLine();
+        }
 
-            if (mainMenuSelect == 2) {
-                receipt(shop);
-            }
-            if (mainMenuSelect == 3) {
-                System.out.println("Thank you, come again.");
-                System.exit(0);
-            }
-
-            System.out.println("Would you like to go back to the main menu? (y/n)");
-            wantsToReturnToMainMenu = input.next();
-        } while (wantsToReturnToMainMenu.equalsIgnoreCase("y"));
-
-    }
-
-//    public static void confirm(Scanner input) {
-//        String y = "y";
-//        String n = "n";
-//
-//    }
-    public static String item(Scanner input) {
-        System.out.println("What would you like to purchase?");
-        String[] itemHolder = new String[100];
-        int i = 0;
-        itemHolder[i] = input.nextLine();
-        i++;
-        return itemHolder[i];
-    }
-
-    public static Double cost(Scanner input) {
-        System.out.println("What is the cost of the item?");
-        Double[] priceHolder = new Double[100];
-        int i = 0;
-        i++;
-        return priceHolder[i] = input.nextDouble();
-    }
-
-    public static int quantity(Scanner input) {
-        System.out.println("How many would you like to buy?");
-        int[] numberOfItemsArray = new int[100];
-        int i = 0;
-        i++;
-        numberOfItemsArray[i] = input.nextInt();
+}
+    public static int mainMenuSelect(Scanner input) {
+        int menuOption = input.nextInt();
         input.nextLine();
-        return numberOfItemsArray[i];
+        return menuOption;
+    }
+
+    public static void showMenu(){
+        System.out.println("Please select an option:");
+        System.out.println("1) Sale");
+        System.out.println("2) Print receipt");
+        System.out.println("3) Exit");
+    }
+
+    public static String item(Scanner input){
+        System.out.println("What would you like to purchase?");
+        return input.nextLine();
+    }
+
+    public static double price(Scanner input){
+        System.out.println("What is the price of the item?");
+        return input.nextDouble();
+    }
+
+    public static int quantity(Scanner input){
+        System.out.println("How many of this item?");
+        input.nextLine();
+        return input.nextInt();
 
     }
 
-    public static void receipt(String shop) {
-        System.out.println(shop);
+    public static void buyAnother(Scanner input){
+        input.nextLine();
+        System.out.println("Would you like to buy something else? (y/n)");
+        String buyAnotherString = input.nextLine();
+        switch (buyAnotherString){
+            case "y":
+                item(input);
+                price(input);
+                quantity(input);
+                buyAnother(input);
+                break;
+            case "n":
+                System.out.println("Would you like to go to the Main Menu? (y/n)");
+                String mainMenuReturn = input.nextLine();
+                switch (mainMenuReturn){
+                    case "y": showMenu();
+                        mainMenuSelect(input);
+                    case "n": break;
+                    default:
+                        System.out.println("Sorry, didn't catch that.");
+                }
+                break;
+            default:
+                System.out.println("Sorry, I didn't get that.");
+        }
     }
-
-    public static String breakingYorN(Scanner input){
-        System.out.println("Would you like to purchase another item? (y/n)");
-       String answer = input.nextLine();
-       return answer;
-    }
-
 
 
 }
